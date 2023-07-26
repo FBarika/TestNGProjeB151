b5282091a04e80dd80d2bc2320d6feb553d9ad06
barika <fatmabarikaince@gmail.com>
2023-07-27 01:25:04 +0200

    Us01_v2

--- src/test/java/tests/US01/TC01_AramaKutusu.java
+++ src/test/java/tests/US01/TC01_AramaKutusu.java
@@ -13,6 +13,7 @@ public class TC01_AramaKutusu {
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
         Page page = new Page();
         page.aramaKutusu.sendKeys("iphone", Keys.ENTER);
+        Driver.closeDriver();
 
     }
 }
